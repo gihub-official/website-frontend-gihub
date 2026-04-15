92a485b0ad283f6e1ab1b40c406b5acaa9241c00
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2026-04-15 15:26:16 +0100

    Research page.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Program from './pages/Program';
 import Platform from './pages/Platform';
+import Research from './pages/Research';
 import './App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/program" element={<Program />} />
         <Route path="/platform" element={<Platform />} />
+        <Route path="/research" element={<Research />} />
       </Routes>
       <Footer />
     </Router>
