d3d616b81f9d1828d5f42c63efefe880b3678506
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2026-05-25 17:25:59 +0100

    Let's see.

--- src/tanstack/useFetchVideoForUser.tsx
+++ src/tanstack/useFetchVideoForUser.tsx
@@ -6,9 +6,11 @@ import type { AxiosError } from "axios";
 export const getVideo = async (tag: string) => {
   try {
     const response = await apiClient.get(`/api/v1/video/tag/${tag}`);
-    console.log(response.data.data)
+    console.log(response.data.data);
     return response.data.data;
   } catch (error) {
+    console.error("Failed to get videos", error);
+    throw error;
   }
 };
 
